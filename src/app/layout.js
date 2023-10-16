6bc95ba44d966c5af2c2b9e972abb2dc8fefe4e9
bellsofaba <bellsofaba@gmail.com>
2023-10-16 14:35:51 -0400

    ThemeToggle

--- src/app/layout.js
+++ src/app/layout.js
@@ -23,7 +23,6 @@ export default function RootLayout({ children }) {
             <Navbar />
             {children}
             <Footer />
-            
             </div>
          </div>
          </ThemeProvider>
--- src/providers/ThemeProvider.jsx
+++ src/providers/ThemeProvider.jsx
@@ -1,14 +1,12 @@
 "use client"
 
-import { ThemeContext } from '@/context/ThemeContext'
-import React, { useContext } from 'react'
+import { ThemeContext } from '@/context/ThemeContext';
+import React, { useContext } from 'react';
 
 const ThemeProvider = ({children}) => {
 
  const {theme} = useContext(ThemeContext)
-  return (
-    <div className={theme}>{children}</div>
-  )
-}
+  return <div className={theme}>{children}</div>
+};
 
 export default ThemeProvider;
