9c93d9b03e6b7dd1f30d8f3a982eb8e90a4db694
bellsofaba <bellsofaba@gmail.com>
2023-10-17 14:41:36 -0400

    Featured

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import styles from "./featured.module.css"
+import Image from 'next/image';
 
-function Featured() {
+const Featured = () => {
   return (
-    <div className={styles.container}>Featured</div>
+    <div className={styles.container}>
+      <h1 className={styles.title}>Explore Technology;<br/>Discover a Wealth of Creative Ideas;<br/>On <b>blogsOfaba.</b></h1>
+      <div className={styles.post}>
+        <div className={styles.imgContainer}>
+          <Image src="/p1.jpeg" alt="" fill className={styles.image} />
+        </div>
+        <div className={styles.textContainer}>
+          <h1 className={styles.postTitle}>Unlocking Tomorrow&apos;s Tech Today: A Distinguished Tech Blog Hub.</h1>
+          <p className={styles.postDesc}>Today&apos;s ever-evolving landscape of tech makes it essential to stay informed with the latest insights, innovations and trends that are shaping the future of technology. Stay Curious. Stay Educated.           
+          </p>
+          <button className={styles.button}>Read More</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
