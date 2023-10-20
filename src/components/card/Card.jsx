1755c5e0181aceaa57f4ed3c2c4e81172e089227
bellsofaba <bellsofaba@gmail.com>
2023-10-20 17:19:36 -0400

    Card Component

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -6,23 +6,22 @@ const Card = () => {
   return (
     <div className={styles.container}>
       <div className={styles.imageContainer}>
-        <Image src="/p1.jpeg" alt="" fill />
+        <Image src="/p1.jpeg" alt="" fill className={styles.image}/>
       </div>
       <div className={styles.textContainer}>
         <div className={styles.detail}>
-          <span className={styles.date}>11.04.2023</span>
+          <span className={styles.date}>11.04.2023 - </span>
           <span className={styles.category}>CULTURE</span>
-          <span>11.04.2023</span>
         </div>
         <Link href="/">
           <h1>5 Must-Have Things to Bring Along With You on Your Next Trip</h1>
         </Link>
-        <p>
+        <p className={styles.desc}>
           Discover the 5 travel essentials you can&rsquo;t leave home without.
           From versatile clothing to handy gadgets, ensure a stress-free and
           enjoyable journey on you next adventure.
         </p>
-        <Link href="/">Read More</Link>
+        <Link href="/" className={styles.link}>Read More</Link>
       </div>
     </div>
   );
