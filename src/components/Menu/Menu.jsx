1eea7a1c3e86fdc288ae72ad1930ae294078e122
bellsofaba <bellsofaba@gmail.com>
2023-10-22 16:58:32 -0400

    Menu Component II

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -24,6 +24,51 @@ function Menu() {
             </div>
           </div>
         </Link>
+        <Link href="/" className={styles.item}>
+          <div className={styles.imageContainer}>
+            <Image src="/p1.jpeg" alt="" fill className={styles.image}/>
+          </div>
+          <div className={styles.textContainer}>
+            <span className={`${styles.category} ${styles.culture}`}>Culture</span>
+            <h3 className={styles.postTitle}>
+              Experience 5th Centuty Cultures Still Triving Today.
+            </h3>
+            <div className={styles.detail}>
+              <span className={styles.username}>bellsOfaba - </span>
+              <span className={styles.date}>10.21.2023</span>
+            </div>
+          </div>
+        </Link>
+        <Link href="/" className={styles.item}>
+          <div className={styles.imageContainer}>
+            <Image src="/p1.jpeg" alt="" fill className={styles.image}/>
+          </div>
+          <div className={styles.textContainer}>
+            <span className={`${styles.category} ${styles.food}`}>Food</span>
+            <h3 className={styles.postTitle}>
+              Unfathomable Deliousness for your Taste Buds.
+            </h3>
+            <div className={styles.detail}>
+              <span className={styles.username}>bellsOfaba - </span>
+              <span className={styles.date}>10.21.2023</span>
+            </div>
+          </div>
+        </Link>
+        <Link href="/" className={styles.item}>
+          <div className={styles.imageContainer}>
+            <Image src="/p1.jpeg" alt="" fill className={styles.image}/>
+          </div>
+          <div className={styles.textContainer}>
+            <span className={`${styles.category} ${styles.fashion}`}>Fashion</span>
+            <h3 className={styles.postTitle}>
+              Experience the Trendiest Apparels Beloved Globally.
+            </h3>
+            <div className={styles.detail}>
+              <span className={styles.username}>bellsOfaba - </span>
+              <span className={styles.date}>10.21.2023</span>
+            </div>
+          </div>
+        </Link>
       </div>
     </div>
   );
