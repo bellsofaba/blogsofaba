9584e6c41aadee3204cc5e53d18218bb3635cfae
bellsofaba <bellsofaba@gmail.com>
2023-10-23 04:57:24 -0400

    Menu Component

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -31,6 +31,80 @@ function Menu() {
           <div className={styles.textContainer}>
             <span className={`${styles.category} ${styles.culture}`}>Culture</span>
             <h3 className={styles.postTitle}>
+              Experience 5th Century Cultures Still Thriving Today.
+            </h3>
+            <div className={styles.detail}>
+              <span className={styles.username}>bellsOfaba - </span>
+              <span className={styles.date}>10.21.2023</span>
+            </div>
+          </div>
+        </Link>
+        <Link href="/" className={styles.item}>
+          <div className={styles.imageContainer}>
+            <Image src="/p1.jpeg" alt="" fill className={styles.image}/>
+          </div>
+          <div className={styles.textContainer}>
+            <span className={`${styles.category} ${styles.food}`}>Food</span>
+            <h3 className={styles.postTitle}>
+              Unfathomable deliousness for your taste Buds.
+            </h3>
+            <div className={styles.detail}>
+              <span className={styles.username}>bellsOfaba - </span>
+              <span className={styles.date}>10.21.2023</span>
+            </div>
+          </div>
+        </Link>
+        <Link href="/" className={styles.item}>
+          <div className={styles.imageContainer}>
+            <Image src="/p1.jpeg" alt="" fill className={styles.image}/>
+          </div>
+          <div className={styles.textContainer}>
+            <span className={`${styles.category} ${styles.fashion}`}>Web3</span>
+            <h3 className={styles.postTitle}>
+              Experience the trendiest dApps revered globally.
+            </h3>
+            <div className={styles.detail}>
+              <span className={styles.username}>bellsOfaba - </span>
+              <span className={styles.date}>10.21.2023</span>
+            </div>
+          </div>
+        </Link>
+      </div>
+
+      <h2 className={styles.subtitle}>Discover by topic</h2>
+      <h1 className={styles.title}>Categories</h1>
+      
+      <h2 className={styles.subtitle}>Chosen by the editor</h2>
+      <h1 className={styles.title}>{"Editor's Pick"}</h1>
+      <div className={styles.categoryList}>
+        <Link href="/blog?cat=style" className={`${styles.categoryItem} ${styles.categoryName}`}
+        >
+          Style
+        </Link>
+      </div>
+      <div className={styles.items}>
+        <Link href="/" className={styles.item}>
+          <div className={styles.imageContainer}>
+            <Image src="/p1.jpeg" alt="" fill className={styles.image}/>
+          </div>
+          <div className={styles.textContainer}>
+            <span className={`${styles.category} ${styles.travel}`}>Travel</span>
+            <h3 className={styles.postTitle}>
+              Travel in style to and from your destination.
+            </h3>
+            <div className={styles.detail}>
+              <span className={styles.username}>bellsOfaba - </span>
+              <span className={styles.date}>10.21.2023</span>
+            </div>
+          </div>
+        </Link>
+        <Link href="/" className={styles.item}>
+          <div className={styles.imageContainer}>
+            <Image src="/p1.jpeg" alt="" fill className={styles.image}/>
+          </div>
+          <div className={styles.textContainer}>
+            <span className={`${styles.category} ${styles.culture}`}>Culture</span>
+            <h3 className={styles.postTitle}>
               Experience 5th Centuty Cultures Still Triving Today.
             </h3>
             <div className={styles.detail}>
