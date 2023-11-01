3d28b1392b88da74676241d4285505bf0e74494c
bellsofaba <bellsofaba@gmail.com>
2023-11-01 18:40:27 -0400

    New Changes

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -3,8 +3,9 @@ import styles from './menu.module.css';
 import Link from 'next/link';
 import Image from 'next/image';
 import MenuPosts from '../MenuPosts/MenuPosts';
+import MenuCategories from "../menuCategories/MenuCategories";
 
-function Menu() {
+const Menu =()=> {
   return (
     <div className={styles.container}>
       <h2 className={styles.subtitle}>{"What's hot?"}</h2>
@@ -12,11 +13,10 @@ function Menu() {
       <MenuPosts withImage={false}/>
       <h2 className={styles.subtitle}>Discover by topic</h2>
       <h1 className={styles.title}>Categories</h1>
-
+      <MenuCategories />
       <h2 className={styles.subtitle}>Chosen by the editor</h2>
       <h1 className={styles.title}>{"Editor's Pick"}</h1>
-  
-     <MenuPosts withImage={true}/>
+     <MenuPosts withImage={true} />
     </div>
   );
 }
--- src/components/menuCategories/MenuCategories.jsx
+++ src/components/menuCategories/MenuCategories.jsx
@@ -1,55 +1,48 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from 'next/link';
+import React from 'react';
+import styles from './menuCategories.module.css';
 
 const MenuCategories = () => {
   return (
-   <div className={styles.categoryList}>
-   <Link
-     href="/blog?cat=style"
-     className={`${styles.categoryItem} ${styles.categoryName}`}
-   >
-     Style
-   </Link>
-       <div className={styles.categoryList}>
-   <Link
-     href="/"
-     className={`${styles.categoryItem} ${styles.categoryName}`}
-   >
-     Fashion
-   </Link>
- </div>
-     <div className={styles.categoryList}>
-   <Link
-     href="/"
-     className={`${styles.categoryItem} ${styles.categoryName}`}
-   >
-     Food
-   </Link>
- </div>
-     <div className={styles.categoryList}>
-   <Link
-     href="/"
-     className={`${styles.categoryItem} ${styles.categoryName}`}
-   >
-     Web3
-   </Link>
- </div>
-     <div className={styles.categoryList}>
-   <Link
-     href="/"
-     className={`${styles.categoryItem} ${styles.categoryName}`}
-   >
-     Culture
-   </Link>
-   <Link
-     href="/"
-     className={`${styles.categoryItem} ${styles.categoryName}`}
-   >
-     Coding
-   </Link>
- </div>
- </div>
-  )
-}
+    <div className={styles.categoryList}>
+      <Link
+        href="/blog?cat=style"
+        className={`${styles.categoryItem} ${styles.style}`}
+      >
+          Style
+      </Link>
+        <Link
+          href="/blog"
+          className={`${styles.categoryItem} ${styles.fashion}`}
+        >
+          Fashion
+        </Link>
+        <Link
+          href="/blog"
+          className={`${styles.categoryItem} ${styles.food}`}
+        >
+          Food
+        </Link>
+        <Link
+          href="/blog"
+          className={`${styles.categoryItem} ${styles.web3}`}
+        >
+          Web3
+        </Link>
+        <Link
+          href="/blog"
+          className={`${styles.categoryItem} ${styles.culture}`}
+        >
+          Culture
+        </Link>
+        <Link
+          href="/blog"
+          className={`${styles.categoryItem} ${styles.coding}`}
+        >
+          Coding
+        </Link>
+      </div>
+  );
+};
 
-export default MenuCategories
+export default MenuCategories;
