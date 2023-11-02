238c9a7935f56377f5c82a9f3888b9c9e0daac81
bellsofaba <bellsofaba@gmail.com>
2023-11-01 20:04:42 -0400

    Menu Categories

--- src/components/menuCategories/MenuCategories.jsx
+++ src/components/menuCategories/MenuCategories.jsx
@@ -7,38 +7,28 @@ const MenuCategories = () => {
     <div className={styles.categoryList}>
       <Link
         href="/blog?cat=style"
-        className={`${styles.categoryItem} ${styles.style}`}
-      >
-          Style
+        className={`${styles.categoryItem} ${styles.data}`}
+        >
+          Data
       </Link>
-        <Link
-          href="/blog"
-          className={`${styles.categoryItem} ${styles.fashion}`}
-        >
-          Fashion
+        <Link href="/blog"
+          className={`${styles.categoryItem} ${styles.defi}`}>
+          DeFi
         </Link>
-        <Link
-          href="/blog"
-          className={`${styles.categoryItem} ${styles.food}`}
-        >
-          Food
+        <Link href="/blog"
+          className={`${styles.categoryItem} ${styles.ai}`}>
+          Artificial Intelligence
         </Link>
-        <Link
-          href="/blog"
-          className={`${styles.categoryItem} ${styles.web3}`}
-        >
+        <Link href="/blog"
+          className={`${styles.categoryItem} ${styles.web3}`}>
           Web3
         </Link>
-        <Link
-          href="/blog"
-          className={`${styles.categoryItem} ${styles.culture}`}
-        >
-          Culture
+        <Link href="/blog"
+          className={`${styles.categoryItem} ${styles.cryptocurrency}`}>
+          Cryptocurrency
         </Link>
-        <Link
-          href="/blog"
-          className={`${styles.categoryItem} ${styles.coding}`}
-        >
+        <Link href="/blog"
+          className={`${styles.categoryItem} ${styles.coding}`}>
           Coding
         </Link>
       </div>
