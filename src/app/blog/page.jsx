b5a789ecc7748e3abc41c888ef7e13c5eb162650
bellsofaba <bellsofaba@gmail.com>
2023-11-06 15:43:37 -0500

    App Blog Page

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import Menu from '@/components/Menu/Menu';
+import CardList from '@/components/cardList/CardList';
+import styles from "./blogPage.module.css";
 
-const BlogPage = () => {
+const BlogPage = ({ searchParams }) => {
+  const page = parseInt(searchParams.page) || 1;
+  const { data } = searchParams;
+
+
   return (
-    <div>BlogPage</div>
-  )
-}
+    <div className={styles.container}>
+      <h1 className={styles.title}>Data Blog</h1>
+      <div className={styles.content}>
+        <CardList page={page} data={data} />
+        <Menu />
+      </div>
+    </div>
+  );
+};
 
 export default BlogPage;
