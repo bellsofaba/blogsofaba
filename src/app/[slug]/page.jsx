e8a696e80b79fc6c407a7077ced49b8e1ef0ff9c
bellsofaba <bellsofaba@gmail.com>
2023-11-13 21:18:29 -0500

    Slug Page

--- src/app/[slug]/page.jsx
+++ src/app/[slug]/page.jsx
@@ -10,7 +10,7 @@ const SinglePage = () => {
           <h1 className={styles.title}>Lorem ipsum dolor sit amet.</h1>
           <div className={styles.user}>
             <div className={styles.userImageContainer}>
-              <Image src="./p1.jpeg" alt="" fill className={styles.image} />
+              <Image src="/p1.jpeg" alt="" fill className={styles.avatar} />
             </div>
             <div className={styles.userTextContainer}>
               <span className={styles.userName}>bellsOfaba</span>
@@ -18,11 +18,24 @@ const SinglePage = () => {
         </div>
       </div>
     </div><div className={styles.imageContainer}>
-        <Image src="./p1.jpeg" alt="" fill className={styles.image} />
+        <Image src="/p1.jpeg" alt="" fill className={styles.image} />
       </div>
       </div>
         <div className={styles.content}>
-          <div className={styles.post}></div>
+          <div className={styles.post}>
+            <div className={styles.description}>
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque similique minima quo alias hic eaque, quos animi ducimus eligendi? Minima.
+              </p>
+              <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet, perspiciatis.</h2>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. In explicabo, pariatur ea aspernatur et soluta beatae minus. Quos, dignissimos repellat!
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. In explicabo, pariatur ea aspernatur et soluta beatae minus. Quos, dignissimos repellat!
+              </p>
+            </div>
+          </div>
           <Menu />
         </div>
       </div>
