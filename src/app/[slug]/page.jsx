66dbd11a9a001ab88b599ebcd4238d1c07f7e030
bellsofaba <bellsofaba@gmail.com>
2023-11-13 23:29:31 -0500

    Comments Component

--- src/app/[slug]/page.jsx
+++ src/app/[slug]/page.jsx
@@ -1,7 +1,7 @@
-import Menu from "@/components/Menu/Menu";
-import styles from "./singlePage.module.css"
-import Image from "next/image";
-import Comments from "@/components/comments/Comments";
+import Menu from '@/components/Menu/Menu';
+import styles from './singlePage.module.css';
+import Image from 'next/image';
+import Comments from '@/components/comments/Comments';
 
 const SinglePage = () => {
   return (
@@ -15,34 +15,44 @@ const SinglePage = () => {
             </div>
             <div className={styles.userTextContainer}>
               <span className={styles.userName}>bellsOfaba</span>
-            <span className={styles.date}>11.10.2023</span>
-        </div>
-      </div>
-    </div><div className={styles.imageContainer}>
-        <Image src="/p1.jpeg" alt="" fill className={styles.image} />
-      </div>
-      </div>
-        <div className={styles.content}>
-          <div className={styles.post}>
-            <div className={styles.description}>
-              <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque similique minima quo alias hic eaque, quos animi ducimus eligendi? Minima.
-              </p>
-              <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet, perspiciatis.</h2>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. In explicabo, pariatur ea aspernatur et soluta beatae minus. Quos, dignissimos repellat!
-              </p>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. In explicabo, pariatur ea aspernatur et soluta beatae minus. Quos, dignissimos repellat!
-              </p>
-            </div>
-            <div className={styles.comment}>
-              <Comments />
+              <span className={styles.date}>11.10.2023</span>
             </div>
           </div>
-          <Menu />
+        </div>
+        <div className={styles.imageContainer}>
+          <Image src="/p1.jpeg" alt="" fill className={styles.image} />
         </div>
       </div>
+      <div className={styles.content}>
+        <div className={styles.post}>
+          <div className={styles.description}>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque
+              similique minima quo alias hic eaque, quos animi ducimus eligendi?
+              Minima.
+            </p>
+            <h2>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet,
+              perspiciatis.
+            </h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. In
+              explicabo, pariatur ea aspernatur et soluta beatae minus. Quos,
+              dignissimos repellat!
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. In
+              explicabo, pariatur ea aspernatur et soluta beatae minus. Quos,
+              dignissimos repellat!
+            </p>
+          </div>
+          <div className={styles.comment}>
+            <Comments />
+          </div>
+        </div>
+        <Menu />
+      </div>
+    </div>
   );
 };
 
--- src/components/comments/Comments.jsx
+++ src/components/comments/Comments.jsx
@@ -1,9 +1,42 @@
-import styles from "./comments.module.css"
+import Link from 'next/link';
+import styles from './comments.module.css';
+import Image from 'next/image';
 
 const Comments = () => {
+  const status = 'authenticated';
   return (
-    <div className={styles.container}>Comments</div>
-  )
-}
+    <div className={styles.container}>
+      <h1 className={styles.title}>Comments</h1>
+      {status === 'authenticated' ? (
+        <div className={styles.write}>
+          <textarea placeholder="Write a comment..." className={styles.input} />
+          <button className={styles.button}>Send</button>
+        </div>
+      ) : (
+        <Link href="./login">Login to write a comment</Link>
+      )}
+      <div className={styles.comments}>
+        <div className={styles.comment}>
+          <div className={styles.user}>
+            <Image
+              src="/p1.jpeg"
+              alt=""
+              width={50}
+              height={50}
+              className={styles.image}
+            />
+            <div className={styles.userInfo}>
+              <span className={styles.username}>bellsOfaba</span>
+              <span className={styles.date}>11.13.2023</span>
+            </div>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias vel veritatis atque similique ex, sequi deleniti saepe ab animi repudiandae?
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Comments;
