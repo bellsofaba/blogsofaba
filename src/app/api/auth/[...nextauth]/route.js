836f3bd1f2c0fa0f05762c51a8d9a0dc57b43466
bellsofaba <bellsofaba@gmail.com>
2023-11-24 16:11:44 -0500

    Updates

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,16 +1,6 @@
+import { authOptions } from "@/utils/auth"
 import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
-import GoogleProvider from "next-auth/providers/google"
 
-export default NextAuth({
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
-    }),
-    GithubProvider({
-     clientId: process.env.GITHUB_ID,
-     clientSecret: process.env.GITHUB_SECRET,
-   }),
-  ],
-})
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,12 @@
 import Navbar from '@/components/navbar/Navbar';
-import './globals.css'
-import { Inter } from 'next/font/google'
+import './globals.css';
+import { Inter } from 'next/font/google';
 import Footer from '@/components/footer/Footer';
 import { ThemeContextProvider } from '@/context/ThemeContext';
 import ThemeProvider from '@/providers/ThemeProvider';
+import AuthProvider from '@/providers/AuthProvider';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'blogsOfaba',
@@ -16,18 +17,20 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ThemeContextProvider>
-        <ThemeProvider>
-        <div className="container">
-          <div className="wrapper">
-            <Navbar />
-            {children}
-            <Footer />
-            </div>
-         </div>
-         </ThemeProvider>
-         </ThemeContextProvider>
-        </body>
+        <AuthProvider>
+          <ThemeContextProvider>
+            <ThemeProvider>
+              <div className="container">
+                <div className="wrapper">
+                  <Navbar />
+                  {children}
+                  <Footer />
+                </div>
+              </div>
+            </ThemeProvider>
+          </ThemeContextProvider>
+        </AuthProvider>
+      </body>
     </html>
   );
 }
